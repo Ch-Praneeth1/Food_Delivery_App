e509c111a2cac0b3921c04235f0b7f030e73c7e5
Ch-Praneeth1 <chpraneethvardhanreddy143@gmail.com>
2023-10-25 19:49:59 +0530

    commit form vs code

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
+
+
+const parent = React.createElement("h1",{id:"parent"},"h1 form react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
