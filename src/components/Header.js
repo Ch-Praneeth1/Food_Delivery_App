9ade5b5943bb6a5986389411f088309520880772
Ch-Praneeth1 <chpraneethvardhanreddy143@gmail.com>
2023-11-25 13:05:10 +0530

    passing the data trough react context to avoid props drilling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 import { LOGO_URL } from '../utils/constants';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import useOnlineStatus from '../utils/useOnlineStatus';
+import UserContext from '../utils/UserContext';
 
 const Header = () =>{
     const [btnValue ,setbtnValue] = useState("Login");
     const onlineStatus = useOnlineStatus();
+
+    const {userLogedIn} = useContext(UserContext);
+    // console.log(userLogedIn);
+
+
     return (
-        <div className="flex bg-blue-200 items-center ">
+        <div className="flex justify-between bg-blue-200 items-center ">
             <div className="w-24 my-2 mx-4">
                 <img src={LOGO_URL} alt=""></img>
             </div>
@@ -31,6 +37,9 @@ const Header = () =>{
                     <button className='loginButton' onClick={ () =>{
                         btnValue === "Login"?setbtnValue("Logout"):setbtnValue("Login")
                     }}>{btnValue}</button>
+                    <li className='mx-6 font-bold'>
+                        {userLogedIn}
+                    </li>                    
                 </ul>
             </div>
         </div>
